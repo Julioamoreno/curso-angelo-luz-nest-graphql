19b753cc3b0db7ac38e6364d088c2138e79e6407
Júlio <julioarmando321@gmail.com>
2021-08-06 17:49:27 -0300

    feat: update and delete user mutations

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateUserInput } from './dto/create-user.input';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { UpdateUserInput } from './dto/update-user.input';
 
 @Resolver('User')
 export class UserResolver {
@@ -13,9 +14,30 @@ export class UserResolver {
     return users;
   }
 
+  @Query(() => User)
+  async user(@Args('id') id: string): Promise<User> {
+    const user = await this.userService.findUserById(id);
+    return user;
+  }
+
   @Mutation(() => User)
   async createUser(@Args('data') data: CreateUserInput): Promise<User> {
     const user = await this.userService.createUser(data);
     return user;
+  }
+
+  @Mutation(() => User)
+  async updateUser(
+    @Args('id') id: string,
+    @Args('data') data: UpdateUserInput,
+  ): Promise<User> {
+    const user = await this.userService.updateUser(id, data);
+    return user;
+  }
+
+  @Mutation(() => User)
+  async deleteUser(@Args('id') id: string): Promise<boolean> {
+    const deleted = await this.userService.deleteUser(id);
+    return deleted;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,13 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
 
 @Injectable()
 export class UserService {
@@ -16,6 +21,14 @@ export class UserService {
     return users;
   }
 
+  async findUserById(id: string): Promise<User> {
+    const user = this.userRepository.findOne(id);
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+    return user;
+  }
+
   async createUser(data: CreateUserInput): Promise<User> {
     const user = await this.userRepository.create(data);
     const userSaved = await this.userRepository.save(user);
@@ -25,5 +38,21 @@ export class UserService {
     }
 
     return userSaved;
+  }
+
+  async updateUser(id: string, data: UpdateUserInput): Promise<User> {
+    const user = await this.findUserById(id);
+    await this.userRepository.update(user, { ...data });
+    const userUpdated = await this.userRepository.create({ ...user, ...data });
+    return userUpdated;
+  }
+
+  async deleteUser(id: string): Promise<boolean> {
+    const user = await this.findUserById(id);
+    const isDeleted = await this.userRepository.delete(user);
+    if (isDeleted) {
+      return true;
+    }
+    return false;
   }
 }
